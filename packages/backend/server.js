7773f84002feee9dff24c5dd8270f06a1b8ea1fe
Miles Smith <miles-smith@users.noreply.github.com>
2020-10-19 15:12:46 +0100

    Reorganize monorepo with Yarn workspaces

--- packages/backend/server.js
+++ packages/backend/server.js
@@ -4,7 +4,7 @@ const products = require('./data/products');
 const app = express();
 
 app.get('/', (request, response) => {
-  response.send('API is running');
+  response.send('API is running...');
 });
 
 app.get('/api/products', (request, response) => {
@@ -13,7 +13,7 @@ app.get('/api/products', (request, response) => {
 
 app.get('/api/products/:id', (request, response) => {
   const product = products.find(product => product._id === request.params.id);
-  
+
   response.json(product);
 });
 
