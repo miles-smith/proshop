b5dabaa41ca6868eb1ff1f5420ea2c5b523f6125
Miles Smith <miles-smith@users.noreply.github.com>
2020-10-19 16:42:06 +0100

    Connect Express to local MongoDB instance

--- packages/backend/server.js
+++ packages/backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 import products from './data/products.js';
 
 dotenv.config();
+connectDB();
 
 const NODE_ENV = process.env.NODE_ENV;
 const PORT = process.env.PORT || 5000;
