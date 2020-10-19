60ac19c8ab005ec237a8991bcf00aa3450db9ed8
Miles Smith <miles-smith@users.noreply.github.com>
2020-10-19 13:54:24 +0100

    Spin up boilerplate Express backend API

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const products = require('./data/products');
+
+const app = express();
+
+app.get('/', (request, response) => {
+  response.send('API is running');
+});
+
+app.get('/api/products', (request, response) => {
+  response.json(products);
+});
+
+app.get('/api/products/:id', (request, response) => {
+  const product = products.find(product => product._id === request.params.id);
+  
+  response.json(product);
+});
+
+app.listen(5000, console.log('Server running on port 5000'));
