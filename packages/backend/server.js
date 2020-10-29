7f35c7b008c6822d32e477e177e49b09580a46de
Miles Smith <miles-smith@users.noreply.github.com>
2020-10-29 16:01:49 +0000

    Add basic backend logging with Morgan.

--- packages/backend/server.js
+++ packages/backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
+import morgan from 'morgan';
 
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
@@ -17,6 +18,10 @@ const NODE_ENV = process.env.NODE_ENV;
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+if(process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json());
 
